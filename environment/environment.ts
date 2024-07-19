048e04145c6c00c14525deaf4793617b18b21bcd
Dima Muzylov <115140474+dimamuzylov@users.noreply.github.com>
2024-07-19 12:55:14 +0300

    feat(env): change telegram web app url

--- environment/environment.ts
+++ environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   tonProviderManifestUrl:
     'https://dimamuzylov.github.io/pyramid-react/tonconnect-manifest.json',
-  telegramWebAppUrl: 'https://t.me/pyramiddddd_bot/Pyramid',
+  telegramWebAppUrl: 'https://t.me/pyramidddd_bot/Pyramid',
 };
